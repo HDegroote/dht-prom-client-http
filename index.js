6dc2ec394031bd556fba5076f6a36268f90105db
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-07-22 13:38:59 +0200

    POC

---
+++ index.js
@@ -0,0 +1,61 @@
+const http = require('http')
+const ReadyResource = require('ready-resource')
+const DhtPromClient = require('dht-prom-client')
+
+class PromClientHttpBridge extends ReadyResource {
+  constructor (dht, scraperPublicKey, alias, scraperSecret, promHttpAddress) {
+    super()
+
+    this.promHttpAddress = promHttpAddress
+
+    this.client = new DhtPromClient(
+      dht,
+      this.getMetrics.bind(this),
+      scraperPublicKey,
+      alias,
+      scraperSecret
+    )
+  }
+
+  get publicKey () {
+    return this.client.publicKey
+  }
+
+  async _open () {
+    await this.client.ready()
+  }
+
+  async close () {
+    // TODO: in progress reqs life cycle on closing
+    // (Either destroy sockets, or wait to finish)
+    await this.client.close()
+  }
+
+  async getMetrics () {
+    return new Promise((resolve, reject) => {
+      http.get(this.promHttpAddress, res => {
+        const status = res.statusCode
+
+        const data = []
+
+        res.on('data', chunk => {
+          data.push(chunk)
+        })
+
+        res.on('end', () => {
+          if (status === 200) {
+            resolve(data.join(''))
+          }
+
+          reject(new Error(
+            `Upstream error (status ${status}): ${data.join('')}`
+          ))
+        })
+      }).on('error', err => {
+        reject(err)
+      })
+    })
+  }
+}
+
+module.exports = PromClientHttpBridge
